554a8b244b044864d9f071d1049cb67ab30a1b8a
Rvaldrez <36698905+Rvaldrez@users.noreply.github.com>
2026-07-02 11:51:01 -0300

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Configurações do Twilio
-define('TWILIO_SID', 'ACa62f55ae13e2208d042d935a7c143777');
-define('TWILIO_AUTH_TOKEN', '1364ad655b5ce53c66c95469b0f2922c');
-define('TWILIO_FROM', 'whatsapp:+14155238886');
-
-
